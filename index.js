0bde7f42ff68281ecd7f8b5adbad98ec1e08eb08
Tabita Ika <eka@istts.ac.id>
2021-05-06 11:32:30 +0700

    push keempat

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ app.get("/budi", function(req, res){
 	res.send("hai Budi!!!");
 });
 
-app.listen(port);
-console.log("Ini lho bro.... "+port);
+app.listen(port, function(){
+	console.log("listening "+port);
+});
