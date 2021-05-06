e8eaec1d56457b4bb700c306cb2f84f0c689f1ea
Tabita Ika <eka@istts.ac.id>
2021-05-06 11:24:36 +0700

    push ketiga

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const barang = require("./routes/barang");
 const app = express();
-const port = process.env.port || 3000;
+const port = 3000;
 
-require("dotenv").config();
-
-app.use(express.urlencoded({extended:true}));
-app.use("/api/barang", barang);
+app.get("/budi", function(req, res){
+	res.send("hai Budi!!!");
+});
 
 app.listen(port);
-console.log("Listening...");
+console.log("Ini lho bro.... "+port);
